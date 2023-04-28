cf23379c10696beee170b046aba530f06f852279
Fernando Nicolás Rouco <rouco.fernando@gmail.com>
2023-04-28 14:41:00 -0300

    Readme

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import './index.css';
 import { App }  from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import configureStore from './redux/store';
 import rootReducer from './redux/reducers';
 import { createStore } from 'redux';
 
 
-// const store = configureStore(rootReducer);
 const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
